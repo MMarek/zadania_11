4125b9278d6ed19146bb0913e4d213ddb3717d1e
MMarek <mildnermarek@gmail.com>
2020-02-23 23:24:17 +0100

    clock modify

--- src/components/task06.js
+++ src/components/task06.js
@@ -4,7 +4,7 @@ class Clock extends Component{
 
     state = {
         date: new Date()
-    }
+    };
 
     componentDidMount() {
         this.intervalId = setInterval(() => {
@@ -30,12 +30,52 @@ class Clock extends Component{
 
 function ClockTime(props) {
     const {date} = props;
-    return <h1>{date.toLocaleString()}</h1>
+    // return <h1>{date.toLocaleString()}</h1>
+    return <h1>
+        <ClockTimeHour date={date}/>
+        <ClockTimeMinute date={date}/>
+        <ClockTimeSecond date={date}/>
+    </h1>
+}
+
+function ClockTimeHour(props) {
+    const {date} = props;
+// return <h1>{date.toLocaleString()}</h1>
+return <span>{date.getHours()}</span>
+}
+
+function ClockTimeMinute(props) {
+    const {date} = props;
+    return <span>{date.getMinutes()}</span>
+}
+
+function ClockTimeSecond(props) {
+    const {date} = props;
+    return <span>{date.getSeconds()}</span>
 }
 
 function ClockDate(props) {
     const {date} = props;
-return <h1>{date.toLocaleString()}</h1>
+    return <h1>
+        <ClockDateYear date={date}/>
+        <ClockDateMonth date={date}/>
+        <ClockDateDay date={date}/>
+    </h1>
+}
+
+function ClockDateYear(props) {
+    const {date} = props;
+    return <span>{date.getFullYear()}</span>
+}
+
+function ClockDateMonth(props) {
+    const {date} = props;
+    return <span>{date.getMonth() + 1}</span>
+}
+
+function ClockDateDay(props) {
+    const {date} = props;
+    return <span>{date.getDate()}</span>
 }
 
 function Task06() {
