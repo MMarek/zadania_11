cb835e2ddc67a0381149ef9f2bb576bdb8966af5
MMarek <mildnermarek@gmail.com>
2020-02-24 22:26:33 +0100

    click game & box correction

--- src/components/task12.js
+++ src/components/task12.js
@@ -1,75 +1,108 @@
 import React, {Component} from "react";
 
-class Box extends Component {
+// class Box extends Component {
+//     state = {
+//         on: false,
+//         off: false,
+//     };
+//
+//     handleMouseOn = () => {
+//         this.setState({
+//             on: true
+//         });
+//         console.log('najazd');
+//     };
+//
+//     handleMouseOff = () => {
+//         this.setState({
+//             on: false
+//         });
+//         console.log('zjazd');
+//     };
+//
+//     handleMouseOn1 = () => {
+//         this.setState({
+//             on: true
+//         });
+//         console.log('najazd');
+//     };
+//
+//     handleMouseOff1 = () => {
+//         this.setState({
+//             off: false
+//         });
+//         console.log('zjazd');
+//     };
+//
+//     render() {
+//         const {on} = this.state;
+//         const {off} = this.state;
+//
+//         let style = {
+//             height: on ? '100px' : '10px',
+//             width: on ? '100px' : '10px',
+//             backgroundColor: on ? 'blue' : 'red',
+//             transition: '5s',
+//             position: 'absolute',
+//             top: '550px'
+//         };
+//
+//         let style1 = {
+//             height: on ? '200px' : '50px',
+//             width: on ? '200px' : '50px',
+//             backgroundColor: on ? 'green' : 'black',
+//             transition: '3s',
+//             position: 'absolute',
+//             left: '300px'
+//         };
+//
+//         return (
+//             <>
+//                 <div style={style}
+//                      onMouseEnter={this.handleMouseOn}
+//                      onMouseLeave={this.handleMouseOff}>
+//                 </div>
+//                 <div style={style1}
+//                      onMouseEnter={this.handleMouseOn1}
+//                      onMouseLeave={this.handleMouseOff1}>
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+class Box extends Component{
     state = {
-        on: false,
-        off: false,
+        backgroundColor: 'green',
+        height: '200px',
+        width: '500px'
     };
 
-    handleMouseOn = () => {
+    handleMouseEnterEvent = () => {
         this.setState({
-            on: true
-        });
-        console.log('najazd');
+            backgroundColor: 'blue',
+            height: '400px'
+        })
     };
 
-    handleMouseOff = () => {
+    handleMouseLeaveEvent = () => {
         this.setState({
-            on: false
-        });
-        console.log('zjazd');
-    };
-
-    handleMouseOn1 = () => {
-        this.setState({
-            on: true
-        });
-        console.log('najazd');
-    };
-
-    handleMouseOff1 = () => {
-        this.setState({
-            off: false
-        });
-        console.log('zjazd');
+            backgroundColor: 'green',
+            height: '200px'
+        })
     };
 
     render() {
-        const {on} = this.state;
-        const {off} = this.state;
-
-        let style = {
-            height: on ? '100px' : '10px',
-            width: on ? '100px' : '10px',
-            backgroundColor: on ? 'blue' : 'red',
-            transition: '5s',
-            position: 'absolute',
-            top: '550px'
-        };
-
-        let style1 = {
-            height: on ? '200px' : '50px',
-            width: on ? '200px' : '50px',
-            backgroundColor: on ? 'green' : 'black',
-            transition: '3s',
-            position: 'absolute',
-            left: '150px'
-        };
-
         return (
-            <>
-                <div style={style}
-                     onMouseEnter={this.handleMouseOn}
-                     onMouseLeave={this.handleMouseOff}>
-                </div>
-                <div style={style1}
-                     onMouseEnter={this.handleMouseOn1}
-                     onMouseLeave={this.handleMouseOff1}>
-                </div>
-            </>
-        )
+            <div
+                onMouseEnter={this.handleMouseEnterEvent}
+                onMouseLeave={this.handleMouseLeaveEvent}
+                style={this.state}>
+            </div>
+        );
     }
 }
+
 
 function Task12() {
     return <Box/>
--- src/components/task14.js
+++ src/components/task14.js
@@ -15,10 +15,34 @@ class SpeedClickGame extends Component{
         clearInterval(this.interval)
     }
 
+    calcuatedTime = () => {
+        this.setState(
+            previousTime => ({
+                time: previousTime.time - 50
+            }),
+            () => {
+                if (this.state.time === 0) {
+                    this.setState({
+                        disabled: true
+                    });
+               clearInterval(this.interval);
+                }
+            })
+    };
+
+    handleClick = () => {
+        this.setState(
+            previousPoints => ({
+                points: previousPoints.points + 1,
+                time: this.props.time - (previousPoints.points + 1) * 50
+            }));
+    };
+
     render() {
         return (
             <div>
                 <h1>Czas: {this.state.time}</h1>
+                <h2>Punkty: {this.state.points}</h2>
             <button onClick={this.handleClick} disabled={this.state.disabled}>Click Me!</button>
             </div>
         )
