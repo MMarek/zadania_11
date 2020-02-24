b735960c22dfb970a32049511690b6b82ccce814
MMarek <mildnermarek@gmail.com>
2020-02-24 08:50:51 +0100

    secound button

--- src/components/task07.js
+++ src/components/task07.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 class Button extends Component {
     state = {
-        texrToShow: null
+        textToShow: null
     };
 
     handleClickAndShowInfo(event, name) {
@@ -12,12 +12,22 @@ class Button extends Component {
         })
     }
 
+    handleClickMathButton = () => {
+        let num1 = parseFloat(prompt("Podaj pierwszą liczbę:"));
+        let num2 = parseFloat(prompt("Podaj drugą liczbę:"));
+
+        this.setState({
+            textToShow: Math.pow(num1,num2)
+        });
+    };
+
     render() {
         let textToShowJsx = this.state.texrToShow ? <h1>{this.state.textToShow}</h1> : null;
         return (
             <>
                 {textToShowJsx}
                 <button onClick={(event) => this.handleClickAndShowInfo(event, "Marek")}>Klik</button>
+                <button onClick={this.handleClickMathButton}>Klik</button>
             </>
         )
     }
