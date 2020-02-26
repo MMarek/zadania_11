fbd743a4220285e899c045305a643871e99551ac
MMarek <mildnermarek@gmail.com>
2020-02-26 11:29:43 +0100

    Updates

---
+++ precache-manifest.dba7a8d6834c92c4ba65967014f58739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f83c7ec7afefb4c0c19202a542cd1e",
+    "url": "/zadania_11/index.html"
+  },
+  {
+    "revision": "1ec0c926b2120d4743d0",
+    "url": "/zadania_11/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0a8cb5725b5e5074e8bb",
+    "url": "/zadania_11/static/js/2.b9ed5dd0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/zadania_11/static/js/2.b9ed5dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec0c926b2120d4743d0",
+    "url": "/zadania_11/static/js/main.71bbaa36.chunk.js"
+  },
+  {
+    "revision": "f5d344d0e4784cf8ddb8",
+    "url": "/zadania_11/static/js/runtime-main.fe298e18.js"
+  }
+]);
